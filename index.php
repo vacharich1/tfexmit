b580ef27a6f8591c725b2baee75e5aaf1a0599a7
jay <jr30149_@hotmail.com>
2017-03-03 07:19:46 +0700

    sdffsdfsd

--- index.php
+++ index.php
@@ -106,7 +106,7 @@ if (!is_null($events['events'])) {
 
 			if($text == '##addroombyjay')
 			{
-					$messages55 = ['type' => 'text','text' => $event['source']['userId']];
+					$messages55 = ['type' => 'text','text' => $event['source']['groupId']];
 					// Make a POST Request to Messaging API to reply to sender
 					$url = 'https://api.line.me/v2/bot/message/reply';
 					$data = [
