50190a9727605ac6cf2a60a804a2a79299691c32
vacharich1 <jr30149_@hotmail.com>
2017-03-05 16:11:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (!is_null($events['events'])) {
 
 			if($text == '##addroombyjay')
 			{
-					$messages55 = ['type' => 'text','text' => $event['source']['groupId']];
+					$messages55 = ['type' => 'text','text' => $event['source']['userId']];
 					// Make a POST Request to Messaging API to reply to sender
 					$url = 'https://api.line.me/v2/bot/message/reply';
 					$data = [
